6c16a4d8e8d5e502bac25b83a906971388926a5f
karei <kareikakareika@yahoo.co.jp>
2018-06-17 19:30:58 +0900

    june17

---
+++ app/src/main/java/com/example/karei/june17/Disire_Photo.kt
@@ -0,0 +1,28 @@
+package com.example.karei.june17
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Disire_Photo : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.disire_photo,container,false)
+    }
+
+
+}
